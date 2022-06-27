1a4ab1c90cc5f999a4e19c63231e30001305b1ac
Liam Schulze <liamschulze16@gmail.com>
2022-06-27 16:45:45 +0200

    Added Method to parse the Ersatzraumplan

--- VertretungsplanConsole/VertretungsplanParser.cs
+++ VertretungsplanConsole/VertretungsplanParser.cs
@@ -113,6 +113,89 @@ namespace VertretungsplanConsole
             return klassen;
         }
 
+        public List<Klasse> ParseErsatzraumplan()
+        {
+            // Load the html document
+            var htmlDoc = LoadHtml();
+
+            // Select the tables from the Document
+            var nodes = htmlDoc.DocumentNode.SelectNodes("//body/table");
+
+            // Create a new list of Klassen
+            var klassen = new List<Klasse>();
+
+            // Create a new instance of Vertretung
+            var vertretung = new Vertretung();
+
+            // If there aren't any nodes return an empty object
+            if (nodes == null)
+                return klassen;
+
+            // Select the last table
+            var ersatzraumplan = nodes[nodes.Count - 1];
+
+            // Select all rows in the table
+            var rows = ersatzraumplan.SelectNodes("tr");
+
+            if (rows == null)
+                rows = ersatzraumplan.SelectNodes("tbody/tr");
+
+            foreach (var row in rows)
+            {
+                row.InnerHtml = row.InnerHtml.Replace("\n", string.Empty);
+
+                // Check if the row is a table heading
+                if (row.InnerHtml.Replace(" ", string.Empty).Contains("<th") && row.InnerText.Replace(" ", string.Empty) != "TrainRaum")
+                    // Create a new klasse and set the content of the table as the name
+                    klassen.Add(new Klasse(row.InnerText.Replace(" ", string.Empty)));
+                else if (row.InnerText.Replace(" ", string.Empty) == "TrainRaum")
+                    break;
+                else
+                {
+                    foreach (var node in row.ChildNodes)
+                    {
+                        node.InnerHtml = node.InnerHtml.Replace("\n", string.Empty);
+
+                        // Add the content of the Vertretungsplan to vertretung
+                        if (node.InnerHtml.Contains("class=\"Eins\""))
+                        {
+                            vertretung.Stunde = node.InnerText.Trim();
+                        }
+                        else if (node.InnerHtml.Contains("class=\"Zwei\""))
+                        {
+                            vertretung.LehrerUndFach = node.InnerText.Replace("&auml;", "ä").Replace("&ouml;", "ö").Replace("&uuml;", "ü").Trim();
+                        }
+                        else if (node.InnerHtml.Contains("class=\"Vier\""))
+                        {
+                            vertretung.VertretungsLehrer = node.InnerText.Replace("&auml;", "ä").Replace("&ouml;", "ö").Replace("&uuml;", "ü").Trim();
+                        }
+                        else if (node.InnerHtml.Contains("class=\"Fuenf\""))
+                        {
+                            vertretung.Message = node.InnerText.Replace("&auml;", "ä").Replace("&ouml;", "ö").Replace("&uuml;", "ü").Trim();
+                        }
+                    }
+
+                    // Add the variable vertretung to the last class of the list
+                    klassen.Last().Vertretungen.Add(vertretung);
+
+                    // Whipe the data from vertretung
+                    vertretung = new Vertretung();
+                }
+            }
+
+            // Check if the ersatzraumplan equals the vertretungsplan
+            if (klassen != ParseVertretungsplan())
+            {
+                // If it isn't the same return it
+                return klassen;
+            }
+            else
+            {
+                // else return null
+                return null;
+            }
+        }
+
         public List<string> ParseFehlendeLehrer()
         {
             // Load html
